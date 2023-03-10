31003e88819729f592a05e0f0013962d89968dc3
ViktorKryvorotov <113522935+ViktorKryvorotov@users.noreply.github.com>
2023-03-10 18:57:50 +0200

    taask-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+return new Promise((resolve, reject) => {
+setTimeout(() => {
+const shouldResolve = Math.random() > 0.3;
+if (shouldResolve) {
+resolve({ position, delay });
+} else {
+reject({ position, delay });
 }
+}, delay);
+});
+}
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit',onFormSubmint) 
+
+function onFormSubmint(event) { 
+  event.preventDefault();
+  
+const delay = Number(form.elements.delay.value);
+const step = Number(form.elements.step.value);
+const amount = Number(form.elements.amount.value);
+
+for (let i = 0; i < amount; i++) {
+const position = i + 1;
+const promiseDelay = delay + i * step;
+createPromise(position, promiseDelay)
+.then(({ position, delay }) => {
+console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+})
+.catch(({ position, delay }) => {
+console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+});
+}
+};
