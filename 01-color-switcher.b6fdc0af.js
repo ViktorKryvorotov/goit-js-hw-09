20f3c8bc8c4aca2db96aad8943014c9e77d4a437
ViktorKryvorotov <113522935+ViktorKryvorotov@users.noreply.github.com>
2023-03-13 09:03:26 +0000

    Deploying to gh-pages from @ ViktorKryvorotov/goit-js-hw-09@41ed8dd17b8545049d2341c2cea3e55479f46fdc 🚀

---
+++ 01-color-switcher.b6fdc0af.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),d=null;t.addEventListener("click",(function(){d=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.disabled=!0,e.addEventListener("click",(function(){clearInterval(d),e.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.b6fdc0af.js.map
